1d87a392b09a79fda9c8920fed90ad08f40270a2
swan-7 <theswansignature@gmail.com>
2022-10-17 15:30:37 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,10 +5,9 @@
  */
 int main(void)
 {
-        char i;
-
-        for (i = 'z'; i >= 'a'; i--)
-                putchar(i);
-        putchar('\n');
-        return (0);
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,12 @@
  */
 int main(void)
 {
-        int i;
-        char hexvalues[] = "0123456789abcdef";
-
-        for (i = 0; i < 16; i++)
-        {
-                putchar(hexvalues[i]);
-        }
-        putchar('\n');
-        return (0);
+int i;
+char hexvalues[] = "0123456789abcdef";
+for (i = 0; i < 16; i++)
+{
+putchar(hexvalues[i]);
+}
+putchar('\n');
+return (0);
 }
