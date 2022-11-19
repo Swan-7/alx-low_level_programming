718c8a9719ecf473366b768483ffec8ed80008b0
swan-7 <theswansignature@gmail.com>
2022-11-18 19:26:37 -0800

    0x0F-function_pointers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,9 +11,8 @@
  */
 void printf_char(va_list list)
 {
-	printf("%c", (char) va_arg(list, int));
+        printf("%c", (char) va_arg(list, int));
 }
-
 /**
  * printf_int - printfs an int from var args
  *
@@ -23,9 +22,8 @@ void printf_char(va_list list)
  */
 void printf_int(va_list list)
 {
-	printf("%d", va_arg(list, int));
+        printf("%d", va_arg(list, int));
 }
-
 /**
  * printf_float - printfs a float from var args
  *
@@ -35,9 +33,8 @@ void printf_int(va_list list)
  */
 void printf_float(va_list list)
 {
-	printf("%f", (float) va_arg(list, double));
+        printf("%f", (float) va_arg(list, double));
 }
-
 /**
  * printf_string - printfs a string from var args
  *
@@ -47,17 +44,15 @@ void printf_float(va_list list)
  */
 void printf_string(va_list list)
 {
-	char str = va_arg(list, char);
+        char *str = va_arg(list, char*);
 
-	while (str != NULL)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
+        while (str != NULL)
+        {
+                printf("%s", str);
+                return;
+        }
+        printf("(nil)");
 }
-
-
 /**
  * print_all - prints various types given a format string for the arguments
  *
@@ -67,30 +62,29 @@ void printf_string(va_list list)
  */
 void print_all(const char * const format, ...)
 {
-	const char *ptr;
-	va_list list;
-	funckey key[4] = { {printf_char, 'c'}, {printf_int, 'i'},
-			   {printf_float, 'f'}, {printf_string, 's'} };
-	int keyind = 0, notfirst = 0;
+        const char *ptr;
+        va_list list;
+        funckey key[4] = { {printf_char, 'c'}, {printf_int, 'i'},
+                           {printf_float, 'f'}, {printf_string, 's'} };
+        int keyind = 0, notfirst = 0;
 
-	ptr = format;
-	va_start(list, format);
-	while (format != NULL && *ptr)
-	{
-		if (key[keyind].spec == *ptr)
-		{
-			if (notfirst)
-				printf(", ");
-			notfirst = 1;
-			key[keyind].f(list);
-			ptr++;
-			keyind = -1;
-		}
-		keyind++;
-		ptr += keyind / 4;
-		keyind %= 4;
-	}
-	printf("\n");
-
-	va_end(list);
+        ptr = format;
+        va_start(list, format);
+        while (format != NULL && *ptr)
+        {
+                if (key[keyind].spec == *ptr)
+                {
+                        if (notfirst)
+                                printf(", ");
+                        notfirst = 1;
+                        key[keyind].f(list);
+                        ptr++;
+                        keyind = -1;
+                }
+                keyind++;
+                ptr += keyind / 4;
+                keyind %= 4;
+        }
+        printf("\n");
+va_end(list);
 }
